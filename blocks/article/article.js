3337e051edc436444c59f6771f2bd276f149ad31
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-08-18 18:33:49 +0530

    Update article.js

--- blocks/article/article.js
+++ blocks/article/article.js
@@ -84,11 +84,11 @@ export default async function decorate(block) {
         (article) => `
           <div class="article-item">
             <div class="article-date">${formatDate(article.date)}</div>
-            <a href="/content/my-website/index/article-content.html?title=${encodeURIComponent(
-              article.title
-            )}" 
-               target="_blank" 
-               class="article-title">${article.title}</a>
+                        <a href="${
+                          article.path
+                        }" data-description="${encodeURIComponent(
+          article.description
+        )}"  class="article-title">${article.title}</a>
           </div>
         `
       )
