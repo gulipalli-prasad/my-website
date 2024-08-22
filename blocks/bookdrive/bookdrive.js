cc6f53ef08e8fa0a0466ac900505c474bab09556
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-08-22 11:21:03 +0530

    Update bookdrive.js

--- blocks/bookdrive/bookdrive.js
+++ blocks/bookdrive/bookdrive.js
@@ -3,19 +3,19 @@ export default function decorate(block) {
     formHeadingEl,
     cityLabelEl,
     carModelEl,
-    transmissionTextEl,
+    transmissionEl,
     manualImageEl,
-    manualTextEl,
+    manualEl,
     automaticImageEl,
-    automaticTextEl,
+    automaticEl,
     testdriveTextEl,
     showroomImageEl,
-    showroomTextEl,
+    showroomEl,
     doorstepImageEl,
-    doorstepTextEl,
-    selectdateTextEl,
-    advanceTextEl,
-    nameTextEl,
+    doorstepEl,
+    selectedDateEl,
+    advanceEl,
+    nameContextEl,
     emailEl,
     mobileEl,
     otpEl,
@@ -28,63 +28,68 @@ export default function decorate(block) {
   const formHeading = formHeadingEl?.textContent?.trim() || "";
   const cityLabel = cityLabelEl?.textContent?.trim() || "";
   const carModel = carModelEl?.textContent?.trim() || "";
-  const transmissionText = transmissionTextEl?.textContent?.trim() || "";
+  const transmission = transmissionEl?.textContent?.trim() || "";
   const manualImageElement = manualImageEl?.querySelector("img");
   const manualImage = manualImageElement?.getAttribute("src")?.trim() || "";
-  const manualText = manualTextEl?.textContent?.trim() || "Manual";
+  const manual = manualEl?.textContent?.trim() || "Manual";
   const automaticImageElement = automaticImageEl?.querySelector("img");
   const automaticImage =
     automaticImageElement?.getAttribute("src")?.trim() || "";
-  const automaticText = automaticTextEl?.textContent?.trim() || "";
+  const automatic = automaticEl?.textContent?.trim() || "";
   const testdriveText = testdriveTextEl?.textContent?.trim() || "";
   const showroomImageElement = showroomImageEl?.querySelector("img");
   const showroomImage = showroomImageElement?.getAttribute("src")?.trim() || "";
-  const showroomText = showroomTextEl?.textContent?.trim() || "Showroom";
+  const showroom = showroomEl?.textContent?.trim() || "Showroom";
   const doorstepImageElement = doorstepImageEl?.querySelector("img");
   const doorstepImage = doorstepImageElement?.getAttribute("src")?.trim() || "";
-  const doorstepText = doorstepTextEl?.textContent?.trim() || "";
-  const selectdateText = selectdateTextEl?.textContent?.trim() || "";
-  const advanceText = advanceTextEl?.textContent?.trim() || "";
-  const nameText = nameTextEl?.textContent?.trim() || "";
-  const emailText = emailEl?.textContent?.trim() || "";
-  const mobileText = mobileEl?.textContent?.trim() || "";
-  const otpText = otpEl?.textContent?.trim() || "";
-  const agreeText = agreeEl?.textContent?.trim() || "";
+  const doorstep = doorstepEl?.textContent?.trim() || "";
+  const selectedDate = selectedDateEl?.textContent?.trim() || "";
+  const advance = advanceEl?.textContent?.trim() || "";
+  const nameContext = nameContextEl?.textContent?.trim() || "";
+  const email = emailEl?.textContent?.trim() || "";
+  const mobile = mobileEl?.textContent?.trim() || "";
+  const otp = otpEl?.textContent?.trim() || "";
+  const agree = agreeEl?.textContent?.trim() || "";
   const conformtext = conformtextEl?.textContent?.trim() || "";
   const anotherTestdriveText = anotherTestdriveEl?.textContent?.trim() || "";
   const submit = submitEl?.textContent?.trim() || "";
 
   block.innerHTML = `
+  <div>
     <h1>${formHeading}</h1>
     <h6>${cityLabel}</h6>
     <h6>${carModel}</h6>
-    <h3>${transmissionText}</h3>
+    <h3>${transmission}</h3>
     <div>
       <img src="${manualImage}" alt="Manual Transmission">
-      <h2>${manualText}</h2>
+      <h2>${manual}</h2>
     </div>
     <div>
       <img src="${automaticImage}" alt="Automatic Transmission">
-      <h2>${automaticText}</h2>
+      <h2>${automatic}</h2>
     </div>
     <h3>${testdriveText}</h3>
     <div>
       <img src="${showroomImage}" alt="Showroom">
-      <h2>${showroomText}</h2>
+      <h2>${showroom}</h2>
     </div>
     <div>
       <img src="${doorstepImage}" alt="Doorstep">
-      <h2>${doorstepText}</h2>
+      <h2>${doorstep}</h2>
     </div>
-    <h6>${selectdateText}</h6>
-    <p>${advanceText}</p>
-    <h3>${nameText}</h3>
-    <h3>${emailText}</h3>
-    <h3>${mobileText}</h3>
-    <h3>${otpText}</h3>
-    <p>${agreeText}</p>
+    <h6>${selectedDate}</h6>
+    <p>${advanceEl}</p>
+</div>
+<div>
+    <h3>${nameContext}</h3>
+    <h3>${email}</h3>
+    <h3>${mobile}</h3>
+    <h3>${otp}</h3>
+    <p>${agree}</p>
     <p>${conformtext}</p>
     <h3>${anotherTestdriveText}</h3>
     <button>${submit}</button>
+</div>
+
   `;
 }
