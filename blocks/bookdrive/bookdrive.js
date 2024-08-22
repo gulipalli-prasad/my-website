e5af2f872cf917bfd8b5560da5e03391644e8594
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-08-22 10:58:45 +0530

    Update bookdrive.js

--- blocks/bookdrive/bookdrive.js
+++ blocks/bookdrive/bookdrive.js
@@ -1,7 +1,58 @@
 export default function decorate(block) {
-  const [formHeadingEl] = block.children;
+  const [
+    formHeadingEl,
+    cityLabelEl,
+    carModelEl,
+    transmissionTextEl,
+    manualImageEl,
+    manualTextEl,
+    automaticImageEl,
+    automaticTextEl,
+    testdriveTextEl,
+    showroomImageEl,
+    showroomTextEl,
+    doorstepImageEl,
+    doorstepTextEl,
+    selectdateTextEl,
+    advanceTextEl,
+    nameTextEl,
+    emailEl,
+    mobileEl,
+    otpEl,
+    agreeEl,
+    conformtextEl,
+    anotherTestdriveEl,
+    submitEl,
+  ] = block.children;
 
   const formHeading = formHeadingEl?.textContent?.trim() || "";
+  const cityLabel = cityLabelEl?.textContent?.trim() || "";
+  const carModel = carModelEl?.textContent?.trim() || "";
+  const transmissionText = transmissionTextEl?.textContent?.trim() || "";
+  const manualImageElement = manualImageEl?.querySelector("img");
+  const manualImage = manualImageElement?.getAttribute("src")?.trim() || "";
+  const manualText = manualTextEl?.textContent?.trim() || "Manual";
+  const automaticImageElement = automaticImageEl?.querySelector("img");
+  const automaticImage =
+    automaticImageElement?.getAttribute("src")?.trim() || "";
+  const automaticText = automaticTextEl?.textContent?.trim() || "";
+  const testdriveText = testdriveTextEl?.textContent?.trim() || "";
+  const showroomImageElement = showroomImageEl?.querySelector("img");
+  const showroomImage = showroomImageElement?.getAttribute("src")?.trim() || "";
+  const showroomText = showroomTextEl?.textContent?.trim() || "Showroom";
+  const doorstepImageElement = doorstepImageEl?.querySelector("img");
+  const doorstepImage = doorstepImageElement?.getAttribute("src")?.trim() || "";
+  const doorstepText = doorstepTextEl?.textContent?.trim() || "";
+  const selectdateText = selectdateTextEl?.textContent?.trim() || "";
+  const advanceText = advanceTextEl?.textContent?.trim() || "";
+  const nameText = nameTextEl?.textContent?.trim() || "";
+  const emailText = emailEl?.textContent?.trim() || "";
+  const mobileText = mobileEl?.textContent?.trim() || "";
+  const otpText = otpEl?.textContent?.trim() || "";
+  const agreeText = agreeEl?.textContent?.trim() || "";
+  const conformtext = conformtextEl?.textContent?.trim() || "";
+  const anotherTestdriveText = anotherTestdriveEl?.textContent?.trim() || "";
+  const submit = submitEl?.textContent?.trim() || "";
 
   block.innerHTML = `
     <h1>${formHeading}</h1>
