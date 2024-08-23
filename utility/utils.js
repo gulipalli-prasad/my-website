8b21730174266d2fa6dcfaf4e79d796f7747b388
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-08-23 16:11:18 +0530

    Update utils.js

--- utility/utils.js
+++ utility/utils.js
@@ -1,8 +1,1 @@
-export async function fetchGraphQL(query, variables = {}) {
-  const response = await fetch("/content/cq:graphql/my-website/endpoint.json", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query, variables }),
-  });
-  return response.json();
-}
+
