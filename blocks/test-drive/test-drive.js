12553a032ac3982513b00028233a023ec63965d9
gulipalli-prasad <gulipalli.prasad@nagarro.com>
2024-08-23 14:40:07 +0530

    updated the changes for testdrive

--- blocks/test-drive/test-drive.js
+++ blocks/test-drive/test-drive.js
@@ -1,16 +1,27 @@
 export default function decorate(block) {
-  const [text1El, text2El] = block.children;
+  function getTestDrive() {
+    const [text1El, text2El] = block.children;
 
-  const text2 = text2El?.textContent?.trim() || "";
-  const text1 = text1El?.textContent?.trim() || "";
+    const text1 = text1El?.textContent?.trim() || "";
+    const text2 = text2El?.textContent?.trim() || "";
 
-  console.log("my text 2 value is: " + text2);
-  console.log("my text 1 value is: " + text1);
+    return {
+      text1,
+      text2,
+    };
+  }
+
+  const textContentData = getTestDrive();
 
   const teaserHtml = `
-    <div>
-      <h4>gulipalli</h4>
-      <h5>prasad</h5>
+    <div class="main-conatiner">
+      <div class="section-1">
+       <h3>${textContentData.text1}</h3>
+      </div>
+
+      <div class="section-2">
+       <h3>${textContentData.text2}</h3>
+      </div>
     </div>
   `;
 
